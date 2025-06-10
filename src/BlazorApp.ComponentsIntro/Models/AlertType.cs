41bb283e51bde785f6ce524efd3ba5d2fb4ff86d
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-06-10 19:57:32 +0200

    Add AlertType enum and enhance Alert component with styles

---
+++ src/BlazorApp.ComponentsIntro/Models/AlertType.cs
@@ -0,0 +1,16 @@
+namespace BlazorApp.ComponentsIntro.Models;
+
+/// <summary>
+/// Bootstrap alert types/colors
+/// </summary>
+public enum AlertType
+{
+    Primary,
+    Secondary,
+    Success,
+    Danger,
+    Warning,
+    Info,
+    Light,
+    Dark
+} 
